be9be599a3e1c232ea235bb7061c0627543e3601
Colin MacDonald <colinmacdonald1213@yahoo.com>
2020-09-17 18:40:14 -0400

    done

--- birdcatcher/main_app/views.py
+++ birdcatcher/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Bird
+from django.views.generic import ListView, DetailView
+from .models import Bird, Toy
 from .forms import FeedingForm
 
 # Create your views here.
@@ -17,10 +18,12 @@ def birds_index(request):
 
 def birds_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
+    toys_bird_doesnt_have = Toy.objects.exclude(id__in = bird.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'birds/detail.html', {
          'bird': bird,
-         'feeding_form': feeding_form
+         'feeding_form': feeding_form,
+         'toys': toys_bird_doesnt_have
     })
 
 def add_feeding(request, bird_id):
@@ -30,6 +33,10 @@ def add_feeding(request, bird_id):
         new_feeding.bird_id = bird_id
         new_feeding.save()
     return redirect('detail', bird_id=bird_id)
+
+def assoc_toy(request, bird_id, toy_id):
+  Bird.objects.get(id=bird_id).toys.add(toy_id)
+  return redirect('detail', bird_id=bird_id)
 
 class BirdCreate(CreateView):
     model = Bird
@@ -43,3 +50,21 @@ class BirdUpdate(UpdateView):
 class BirdDelete(DeleteView):
     model = Bird
     success_url = '/birds/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
